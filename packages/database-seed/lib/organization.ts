502a757c9b5f5737ed8e2af8f3d304dda47d5dfc
Alex Wine <alexwine36@gmail.com>
2025-01-28 19:18:00 -0700

    fix: update seeds

---
+++ packages/database-seed/lib/organization.ts
@@ -0,0 +1,48 @@
+import { faker } from "@faker-js/faker";
+import type { Organization, Prisma, PrismaClient } from "@repo/database";
+
+export const getOrganization = async (
+  data: Omit<Prisma.OrganizationCreateInput, "type">,
+  prisma: PrismaClient
+) => {
+  const org = await prisma.organization.findFirst({
+    where: {
+      name: data.name,
+    },
+  });
+  if (org) {
+    return org;
+  }
+  return prisma.organization.create({
+    data: {
+      ...data,
+      members: {
+        create: {
+          role: "OWNER",
+          email: "alexwine36@gmail.com",
+        },
+      },
+    },
+  });
+};
+
+export const seedMembers = async (org: Organization, prisma: PrismaClient) => {
+  const members = await prisma.member.count({
+    where: {
+      organizationId: org.id,
+    },
+  });
+  if (members > 5) {
+    return;
+  }
+
+  new Array(faker.number.int({ min: 5, max: 10 })).fill(0).map(async () => {
+    await prisma.member.create({
+      data: {
+        role: faker.helpers.arrayElement(["ADMIN", "MEMBER"]),
+        email: faker.internet.email(),
+        organizationId: org.id,
+      },
+    });
+  });
+};
