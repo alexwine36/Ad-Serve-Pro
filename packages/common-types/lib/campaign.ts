06fb95910c64131e9cb72506cef493743c763c81
Alex Wine <alexwine36@gmail.com>
2025-01-30 16:56:32 -0700

    fix: udpated default location for campaign

--- packages/common-types/lib/campaign.ts
+++ packages/common-types/lib/campaign.ts
@@ -1,6 +1,6 @@
 import { add } from 'date-fns';
 import { z } from 'zod';
-import { CampaignSchema } from './generated';
+import { CampaignSchema, CampaignStatusSchema } from './generated';
 import { getStartOfDate } from './utils/get-normalized-date';
 export const CampaignTargeting = z.object({
   pathIncludes: z.string().optional(),
@@ -10,6 +10,12 @@ export const CampaignData = CampaignSchema.extend({
   // Update base types here
   targeting: CampaignTargeting.optional().default({}),
   budget: z.coerce.number().default(0),
+  name: z.string().min(2).default(''),
+  startDate: z.date().default(() => getStartOfDate(new Date())),
+  endDate: z
+    .date()
+    .default(() => add(getStartOfDate(new Date()), { months: 1 })),
+  status: CampaignStatusSchema.default('DRAFT'),
 });
 
 export type CampaignData = z.infer<typeof CampaignData>;
@@ -24,14 +30,15 @@ export type CampaignUpdateInput = z.infer<typeof CampaignUpdateInput>;
 
 export const CampaignInput = CampaignUpdateInput.partial({
   id: true,
-}).default({
-  // Define default values here
-  name: '',
-  startDate: getStartOfDate(new Date()),
-  endDate: add(getStartOfDate(new Date()), { months: 1 }),
-  status: 'DRAFT',
-  companyId: '',
-  targeting: {},
-  budget: 0,
 });
+// .default({
+//   // Define default values here
+//   name: '',
+//   startDate: getStartOfDate(new Date()),
+//   endDate: add(getStartOfDate(new Date()), { months: 1 }),
+//   status: 'DRAFT',
+//   companyId: '',
+//   targeting: {},
+//   budget: 0,
+// });
 export type CampaignInput = z.infer<typeof CampaignInput>;
