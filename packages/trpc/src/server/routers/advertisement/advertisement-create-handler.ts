c30e9752486613a3ade1a0f574d4a36d1b0132b7
Alex Wine <alexwine36@gmail.com>
2025-02-03 14:52:09 -0700

    fix: update lint

--- packages/trpc/src/server/routers/advertisement/advertisement-create-handler.ts
+++ packages/trpc/src/server/routers/advertisement/advertisement-create-handler.ts
@@ -1,21 +1,24 @@
 import type { TRPCContextInnerWithSession } from '@/server/create-context';
-import type { AdvertisementCreateSchema } from './advertisement-create-schema'
-import { z } from 'zod';
-import { AdvertisementData } from "@repo/common-types";
+import { AdvertisementData } from '@repo/common-types';
+import type { AdvertisementCreateSchema } from './advertisement-create-schema';
 
 type AdvertisementCreateOptions = {
-    ctx: TRPCContextInnerWithSession;
-    input: AdvertisementCreateSchema;
-}
+  ctx: TRPCContextInnerWithSession;
+  input: AdvertisementCreateSchema;
+};
 
-export const advertisementCreateHandler = async ({ ctx, input }: AdvertisementCreateOptions) => {
+export const advertisementCreateHandler = async ({
+  ctx,
+  input,
+}: AdvertisementCreateOptions) => {
+  const { prisma, session } = ctx;
 
-    const { prisma, session } = ctx;
-    
-    const res = await prisma.advertisement.create({
-          data: {...input}
-        });
-    return AdvertisementData.parse(res);
-}
+  const res = await prisma.advertisement.create({
+    data: { ...input },
+  });
+  return AdvertisementData.parse(res);
+};
 
-export type AdvertisementCreateResponse = Awaited<ReturnType<typeof advertisementCreateHandler>>;
+export type AdvertisementCreateResponse = Awaited<
+  ReturnType<typeof advertisementCreateHandler>
+>;
