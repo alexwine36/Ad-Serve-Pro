1d7455668e1327a1786bbd78b72c976bee4fc25c
Alex Wine <alexwine36@gmail.com>
2025-02-13 15:17:48 -0700

    add changeset

--- apps/app/app/(marketing)/layout.tsx
+++ apps/app/app/(marketing)/layout.tsx
@@ -6,17 +6,28 @@ import { ScrollToTop } from './components/scroll-to-top';
 type MarketingLayoutProps = {
   readonly children: ReactNode;
 };
+
+const getDefaultCompany = async () => {
+  try {
+    const defaultOrg = await database.organization.findFirst({
+      where: {
+        name: 'Acme Inc.',
+      },
+    });
+    return defaultOrg?.id;
+  } catch (error) {
+    console.error('Error fetching default company', error);
+    return '';
+  }
+};
+
 const MarketingLayout = async ({ children }: MarketingLayoutProps) => {
-  const defaultOrg = await database.organization.findFirst({
-    where: {
-      name: 'Acme Inc.',
-    },
-  });
+  const defaultOrgId = await getDefaultCompany();
   return (
     <>
       <script
         id="ad-analytics"
-        data-org-id={defaultOrg?.id}
+        data-org-id={defaultOrgId}
         data-debug="true"
         // data-endpoint="/api/analytics"
         src="/analytics/client.js"
